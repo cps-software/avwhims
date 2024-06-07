e244738237cbd19319347adf61580fce789c707e
Chuck Sylvester <chuck.sylvester@outlook.com>
2024-06-07 14:45:26 -0400

    Build out proper Patient main and child tables

--- _dbscript/create/create_tbl_patient.sql
+++ _dbscript/create/create_tbl_patient.sql
@@ -1,17 +1,17 @@
 -- Create patient table
--- describe table
 
 USE avwhims;
 
 CREATE TABLE patient
 (
   patient_id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	patient_rems_id varchar(12) NULL,
-	patient_nccc_id varchar(10) NULL,
+	patient_va_icn varchar(32) NULL,
 	patient_ssn varchar(10) NULL,
 	patient_firstname varchar(50) NULL,
 	patient_lastname varchar(50) NULL,
 	patient_middlename varchar(50) NULL,
+	patient_maiden_name varchar(50) NULL,
+	patient_gender_code varchar(16) NULL,
 	patient_race varchar(128) NULL,
 	patient_ethnicity varchar(128) NULL,
 	patient_date_of_birth datetime NOT NULL,
@@ -19,16 +19,18 @@ CREATE TABLE patient
 	patient_zipcode varchar(32) NULL,
 	patient_icn varchar(128) NULL,
 	patient_dfn varchar(128) NULL,
-	created datetime NOT NULL,
+	patient_gravidity int NOT NULL,
+	patient_parity int NOT NULL,
+	created_date datetime NOT NULL,
 	created_by_id int NOT NULL,
-	last_modified datetime NOT NULL,
+	last_modified_date datetime NOT NULL,
 	last_modified_by_id int NOT NULL,
 	file_location_id int NOT NULL,
 	patient_gender_id int NOT NULL,
 	mpi_sync bit NOT NULL,
     INDEX
     (
-        patient_rems_id
+        patient_va_icn
     ),
     INDEX
     (
@@ -40,6 +42,6 @@ CREATE TABLE patient
     ),
     INDEX
     (
-        patient_icn
+        patient_dfn
     )
 );
---
+++ _dbscript/insert/insert_row_patient_phone.sql
@@ -0,0 +1,9 @@
+-- Populate table: patient_phone
+
+USE avwhims;
+
+INSERT INTO patient_phone (patient_id, patient_phone_type, patient_phone_priority, patient_phone_country, patient_phone_area, patient_phone_number)
+VALUES
+('1', '1', '1', '1', '808', '5551111'),
+('1', '2', '2', '1', '808', '5552222'),
+('1', '3', '2', '1', '808', '5553333');
